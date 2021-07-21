42cc0575ac4e59467114dfb66f6abf0b96262403
lidorSharabi <lidor206@gmail.com>
2021-07-22 00:18:03 +0300

    table comp

--- apps/test/src/app/components/account-balance/account-balance.component.ts
+++ apps/test/src/app/components/account-balance/account-balance.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Input } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AccountData } from '../../models/data';
 
 @Component({
@@ -8,19 +8,15 @@ import { AccountData } from '../../models/data';
   styleUrls: ['./account-balance.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountBalanceComponent implements OnInit {
+export class AccountBalanceComponent {
   currentBalance: number | undefined;
   accumulateAmountTransactions: number = 0;
 
   @Input() set accountData(value: AccountData) {
-    this.currentBalance = value.currentBalance;
-    this.accumulateAmountTransactions = 0;
-    value.transactions.forEach(e => this.accumulateAmountTransactions += e.amount? e.amount: 0)
+    if (value!!) {
+      this.currentBalance = value.currentBalance;
+      this.accumulateAmountTransactions = 0;
+      value.transactions.forEach(e => this.accumulateAmountTransactions += e.amount ? e.amount : 0)
+    }
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
--- apps/test/src/app/components/filter-transactions/filter-transactions.component.ts
+++ apps/test/src/app/components/filter-transactions/filter-transactions.component.ts
@@ -20,20 +20,24 @@ export class FilterTransactionsComponent {
   @Input() set dateRangeList(value: DateRange[]) {
     this.dateRanges = value;
     this.selectedDateRange = this.dateRanges[0];
+    this.filterChanged();
   }
 
   @Input() set clientsAccountsList(value: ClientAccount[]) {
     this.clientsAccounts = value;
     this.selectedAccount = this.clientsAccounts[0];
+    this.filterChanged();
   }
 
   filterChanged() {
-    let req = {
-      id: this.selectedAccount?.id,
-      startDate: this.calculateStartDate(),
-      endDate: new Date()
-    } as FilterRequest;
-    this.newFilterRequest.emit(req);
+    if (this.selectedAccount?.id && this.selectedDateRange?.value) {
+      let req = {
+        id: this.selectedAccount?.id,
+        startDate: this.calculateStartDate(),
+        endDate: new Date()
+      } as FilterRequest;
+      this.newFilterRequest.emit(req);
+    }
   }
 
   calculateStartDate() {
